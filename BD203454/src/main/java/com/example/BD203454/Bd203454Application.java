309977cb249fee705f217102b78050f42d3ff2bb
203454 <77992720+203454@users.noreply.github.com>
2021-10-27 10:41:03 -0500

    Add files via upload

---
+++ BD203454/src/main/java/com/example/BD203454/Bd203454Application.java
@@ -0,0 +1,13 @@
+package com.example.BD203454;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bd203454Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bd203454Application.class, args);
+	}
+
+}
---
+++ BD203454/src/test/java/com/example/BD203454/Bd203454ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BD203454;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bd203454ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
