7c9981beb270785ea6cd46d956c46e7f0eaa92ed
Francisco Omar Franco Espinosa <fofe2803@gmail.com>
2021-10-29 15:05:02 -0500

    Actualizacion de DuenioController
    Se quitaron cosas innnecesarias en el controlador del dueño.

--- BD203454/src/main/java/com/example/BD203454/controller/DuenioController.java
+++ BD203454/src/main/java/com/example/BD203454/controller/DuenioController.java
@@ -20,34 +20,13 @@ public class DuenioController {
 
     @GetMapping(value = "/listaDuenios")
     public List<Duenio> getListDuenio(){
+
         return duenioRepository.findAll();
     }
 
     @PostMapping(value ="/tipo/add")
-    public Duenio addTipo(@RequestBody Duenio tipo){
-        return duenioRepository.save(tipo);
+    public Duenio addTipo(@RequestBody Duenio duenio){
+        return duenioRepository.save(duenio);
     }
 
-
-    /*@PostMapping(value = "/tipo/update")
-    public Duenio updateTipo(@RequestBody Duenio tipo){
-        Duenio t = duenioRepository.findByTipoId(tipo.getId_duenio());
-        if(t != null){
-            t.setDireccion(tipo.getDireccion());
-            return duenioRepository.save(t);
-        }else {
-            return null;
-        }
-    }
-
-    @PostMapping(value = "/tipo/delete")
-    public Boolean deleteTipo(@RequestBody Tipo tipo) {
-        Tipo t = tipoRepository.findByTipoId(tipo.getTipoId());
-        if (t != null) {
-            tipoRepository.delete(t);
-            return true;
-        } else {
-            return false;
-        }
-    }*/
 }
